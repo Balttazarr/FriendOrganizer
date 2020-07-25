84c8a665d1c9db3054d449bf29bce909f54d005e
Balttazarr <felekmakuta@wp.pl>
2020-07-25 13:32:05 +0200

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //TODO LOAD DATA FROM DB
+            yield return new Friend { FirstName = "Tomasz", LastName = "Lyzwa" };
+            yield return new Friend { FirstName = "Faustyna", LastName = "Knasiecka" };
+
+            yield return new Friend { FirstName = "Maciej", LastName = "Cieszynski" };
+
+            yield return new Friend { FirstName = "Wiktor", LastName = "Kucharski" };
+
+        }
+    }
+}
